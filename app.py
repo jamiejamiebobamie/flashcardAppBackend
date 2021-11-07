0103f537ea10c5747aefee5a79eaf5e38e89e000
jamiejamiebobamie <jmccrory@vt.edu>
2021-11-07 01:06:37 -0400

    added changes.

--- app.py
+++ app.py
@@ -55,11 +55,9 @@ def _main_post():
         Topic = request.form.getlist('Topic')[0]
         front = request.form.getlist('front')[0]
         back = request.form.getlist('back')[0]
-
         # right now inserted_ok is always True
         # inserted_ok = request.headers.get('inserted')
         inserted_ok = True
-
         return render_template('index.html', Inserted=inserted_ok, Domain=Domain, Subdomain=Subdomain, Topic=Topic)
     else:
         return render_template('login.html')
@@ -83,8 +81,6 @@ def add_card_to_db(Domain,Subdomain,Topic,front,back):
 def add_card():
     is_logged_in = request.cookies.get('loggedin?')
     if is_logged_in == str(os.environ.get('is_logged_in')):
-        # db = mongo.db
-        # cardstacks = db.cardstacks
         Domain = request.form.getlist('Domain')[0]
         Subdomain = request.form.getlist('Subdomain')[0]
         Topic = request.form.getlist('Topic')[0]
@@ -99,21 +95,11 @@ def add_card():
         inserted_ok = False
         if all_fields_contain_content:
             inserted_ok = add_card_to_db(Domain,Subdomain,Topic,front,back)
-
         # need to find a way to send along the status of the insertion to _main_post
         # request.headers.set("inserted", inserted_ok)
-
         return redirect(url_for('._main_post'), code=307)
     else:
         return render_template('login.html')
-
-# temporary route to update db with the 'flagged' boolean.
-# @app.route('/update-db')
-# def add_flagged():
-#     db = mongo.db
-#     cardstacks = db.cardstacks
-#     success = cardstacks.update_many( {}, {  "$set": { "flagged": "false"}  }).acknowledged
-#     return {"success":success}
 
 # web scrape quizlet.com
 # must log in first and copy the url path of the flashcards you want.
@@ -124,7 +110,6 @@ def quizlet_url_form():
         return render_template('quizlet.html')
     else:
         return render_template('login.html')
-
 
 # web scrape quizlet.com
 # must log in first and copy the url path of the flashcards you want.
@@ -157,14 +142,10 @@ def get_flashcards():
                 <span class="TermText notranslate lang-en”> term
                 <span class="TermText notranslate lang-en”> definition
             """
-
             terms_defs = soup.findAll("span", {"class": "TermText notranslate lang-en"})
-
             flashcard_documents = []
             front = None
-            print(terms_defs,soup)
             if terms_defs:
-                print('test2')
                 for i in range(len(terms_defs)):
                     terms_defs[i] = terms_defs[i].prettify()
                     # grab the content between the <span></span> tags
@@ -172,7 +153,6 @@ def get_flashcards():
                     if len(content)>1:
                         content = [c for c in content if c != ' <br/>']
                     content = "".join(content)
-                    # print(content)
                     if not front:
                         # need to do more testing, but it appears there is a leading
                             # space in front of the terms and definitions that needs
@@ -181,7 +161,6 @@ def get_flashcards():
                     else:
                         # strip the leading space.
                         back = content[1:]
-                        # print(Domain,Subdomain,Topic,front,back)
                         new_document = {
                                 "Domain" : Domain,
                                 "Subdomain" : Subdomain,
@@ -192,8 +171,6 @@ def get_flashcards():
                             }
                         flashcard_documents.append(new_document)
                         front = None
-                print(url)
-                print(flashcard_documents[0])
                 inserted_ok = cardstacks.insert_many(flashcard_documents).acknowledged
         return render_template('quizlet.html', inserted_ok=inserted_ok)
     else:
@@ -215,21 +192,17 @@ def submit_delete_form():
         Domain = request.form.getlist('Domain')[0]
         Subdomain = request.form.getlist('Subdomain')[0]
         Topic = request.form.getlist('Topic')[0]
-        # print(Domain,Subdomain,Topic)
-
         all_fields_contain_content = (len(Domain) and
                                      len(Subdomain) and
                                      len(Topic))
         if all_fields_contain_content:
             db = mongo.db
             cardstacks = db.cardstacks
-            print(Domain,Subdomain,Topic)
             d = cardstacks.delete_many( {
                    "Domain":Domain,
                    "Subdomain":Subdomain,
                    "Topic":Topic,
                    } )
-            print(d.deleted_count)
         return render_template('delete.html')
     else:
         return render_template('login.html')
@@ -242,32 +215,51 @@ def query_cards():
     # this contains which subjects the user wants flashcards on.
     subject_data = request.get_json()
     filtered_cards = []
+    # if a user asks for FLAGGED_CARDS, to avoid duplicates toggle this boolean.
+    include_flagged = not any([True if len(string.split("*")) < 2 else False for string in subject_data])
     # iterate through the subject_data
     for string in subject_data:
         # each subject_data entry is a string separated by '*' of:
         # 'Domain*Subdomain*Topic'
         terms_to_match = string.split("*")
-        cards = cardstacks.find({
-            "Domain":terms_to_match[0],
-            "Subdomain":terms_to_match[1],
-            "Topic":terms_to_match[2],
-            })
-        if cards:
-            for card in cards:
-                # probably a better way to do this, but create a new card object
-                    # that does not contain the object id from the database
-                    # as this throws an error when returning the filtered_cards
-                    # as JSON.
-                card = {
-                        "Domain" : card["Domain"],
-                        "Subdomain" : card["Subdomain"],
-                        "Topic" : card["Topic"],
-                        "front" : card["front"],
-                        "back" : card["back"],
-                        "flagged" : card["flagged"]
-                        }
-                # add the new card object to the filtered_cards array
-                filtered_cards.append(card)
+        if len(terms_to_match) > 2:
+            cards = cardstacks.find({
+                "Domain":terms_to_match[0],
+                "Subdomain":terms_to_match[1],
+                "Topic":terms_to_match[2],
+                })
+            if cards:
+                for card in cards:
+                    # probably a better way to do this, but create a new card object
+                        # that does not contain the object id from the database
+                        # as this throws an error when returning the filtered_cards
+                        # as JSON.
+                    if include_flagged or card["flagged"] == "false":
+                        card = {
+                                "Domain" : card["Domain"],
+                                "Subdomain" : card["Subdomain"],
+                                "Topic" : card["Topic"],
+                                "front" : card["front"],
+                                "back" : card["back"],
+                                "flagged" : card["flagged"]
+                                }
+                        # add the new card object to the filtered_cards array
+                        filtered_cards.append(card)
+            # FLAGGED_CARDS
+        else:
+            cards = cardstacks.find({"flagged":"true"})
+            if cards:
+                for card in cards:
+                    card = {
+                            "Domain" : card["Domain"],
+                            "Subdomain" : card["Subdomain"],
+                            "Topic" : card["Topic"],
+                            "front" : card["front"],
+                            "back" : card["back"],
+                            "flagged" : card["flagged"]
+                            }
+                    # add the new card object to the filtered_cards array
+                    filtered_cards.append(card)
     # return the filtered cards to the frontend.
     return { "cards": filtered_cards }
 
@@ -280,8 +272,8 @@ def query_tabs():
         # Subdomain.
     # create the appropriate data structure for the frontend so users can query
         # the database with the specific topics they want to study.
-    tabs = []
     all_domains = cardstacks.distinct('Domain')
+    tabs = []
     for i, each_domain in enumerate(all_domains):
         tabs.append({'tabName':each_domain,'content':[]})
         all_subdomains_of_this_domain = cardstacks.find({
@@ -298,8 +290,7 @@ def query_tabs():
             for each_topic in all_topics_of_this_subdomains:
                 tabs[i]['content'][j]['content'].append({'tabName':each_topic})
 
-    return {'tabs':tabs}
-
+    return {'tabs':[{'tabName':'FLAGGED_CARDS'}] + tabs}
 
 @app.route('/api/v1/flagged', methods=['POST'])
 def flag_card():
@@ -312,15 +303,6 @@ def flag_card():
         flagged = "true"
     else:
         flagged = "false"
-    # card = cardstacks.find_one({
-    #     "Domain":flagged_card_data["Domain"],
-    #     "Subdomain":flagged_card_data["Subdomain"],
-    #     "Topic":flagged_card_data["Topic"],
-    #     "front":flagged_card_data["front"],
-    #     })
-    # success = False
-    # if card:
-    #     print(card)
     success = cardstacks.update_one( {
         "Domain":flagged_card_data["Domain"],
         "Subdomain":flagged_card_data["Subdomain"],
@@ -328,7 +310,6 @@ def flag_card():
         "front":flagged_card_data["front"],
         }, {  "$set": { "flagged": flagged }  }).acknowledged
     return { "success": success }
-
 
 if __name__ == '__main__':
     port = os.getenv("PORT", 7000)
